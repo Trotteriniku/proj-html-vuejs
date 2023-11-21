38550dac2b10aeabdd713109bff55e41b4d930ab
Trotteriniku <stefanozidda3@gmail.com>
2023-11-21 17:33:09 +0100

    Scaffolding

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,20 @@
 
 <script>
   export default {
+    import CardNComponent from './components/CardNComponent.vue';
+    import CardsComponentList from './components/CardsComponentList.vue';
+    import FooterComponent from './components/FooterComponent.vue';
+    import HalfCardComponent from './components/HalfCardComponent.vue';
+    import HeaderComponent from './components/HeaderComponent.vue';
+    import NavComponent from './components/NavComponent.vue';
     name:'App',
     components:{
-
+      CardNComponent,
+      CardsComponentList,
+      FooterComponent,
+      HalfCardComponent,
+      HeaderComponent,
+      NavComponent
     },
     data(){
       return{
