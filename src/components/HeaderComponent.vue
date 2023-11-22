68b7d34ba3a980d0f1ff417b54306e42c7cba83a
Trotteriniku <stefanozidda3@gmail.com>
2023-11-22 16:03:51 +0100

    NavComponent

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,15 +1,23 @@
 <template>
-    <div>
-
-    </div>
+    <!-- header posizionato in sticky per funzioni future -->
+    <header class=" position-sticky ">
+        <NavComponent/>
+    </header>
+    
 </template>
 
 <script>
+import NavComponent from './NavComponent.vue'
     export default {
-        name:'HeaderComponent'
+        name:'HeaderComponent',
+        components:{
+            NavComponent
+        }
     }
 </script>
 
 <style lang="scss" scoped>
-
+    header{
+        height: 112px;
+    }
 </style>
--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -1,7 +1,59 @@
 <template>
-    <div>
+    <!-- container totale -->
+    <nav class="container">
 
-    </div>
+        <div class="row align-items-center align-content-center justify-content-between py-4 ">
+        <!-- logo -->
+            <a href="" class=" col-auto">
+                <img src="../assets/imagesNotDinamic/avada-marketing-logo.png" alt="avada logo">
+            </a>
+        <!-- navbar menu list -->
+            <ul class="col-auto d-flex  gap-4 m-0 list-unstyled  align-items-center align-content-center">
+                    <li class="">
+                        <a href="" class=" nav-link ">
+                            <span class=" fw-medium fs-5 ">Home</span>
+                            <span></span>
+                        </a>
+                    </li>
+                    <li class="   ">
+                        <a href="" class=" nav-link ">
+                            <span class=" fw-medium fs-5 ">Services</span>
+                            <span></span>
+                        </a>
+                    </li>
+                    <li class="   ">
+                        <a href="" class=" nav-link ">
+                            <span class=" fw-medium fs-5 ">Why Us</span>
+                        </a>
+                    </li>
+                    <li class="   ">
+                        <a href="" class=" nav-link ">
+                            <span class=" fw-medium fs-5 ">Case Studies</span>
+                        </a>
+                    </li>
+                    <li class="   ">
+                        <a href="" class=" nav-link ">
+                            <span class=" fw-medium fs-5 ">About</span>
+                        </a>
+                    </li>
+                    <li class="   ">
+                        <a href="" class=" nav-link ">
+                            <span class=" fw-medium fs-5 ">Blog</span>
+                        </a>
+                    </li>
+            </ul>
+                
+        <!-- phone-number -->
+            <div class=" col-auto " >
+                    <span class="me-3"><i class="fa-solid fa-phone"></i></span>
+                    <span class="fs-5 fw-medium  ">(555) 802-1234</span>
+            </div>
+        
+        <!-- Quote Button -->
+            <a class="btn btn-outline-success col-auto my-btn " ><span class="mx-3 fw-medium ">Free Quote</span></a>
+            
+        </div>
+    </nav>
 </template>
 
 <script>
@@ -11,5 +63,40 @@
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
 
+    li:first-child{
+        color:  #F86011;
+
+    }
+
+    i{
+        color:  #F86011;
+    }
+
+    .my-btn{
+        padding: 13px 14px;
+        border-radius: 12px;
+
+        span{
+            font-size: 1.15em;
+        }
+    }
+.btn-outline-success{
+    --bs-btn-color: #F86011;
+    --bs-btn-border-color: $-my-primary ;
+    --bs-btn-hover-color: #fff;
+    --bs-btn-hover-bg: #F86011;
+    --bs-btn-hover-border-color: #F86011;
+    --bs-btn-focus-shadow-rgb: 25, 135, 84;
+    --bs-btn-active-color: #fff;
+    --bs-btn-active-bg: #F86011;
+    --bs-btn-active-border-color: #F86011;
+    --bs-btn-active-shadow: inset 0 3px 5px rgba(0, 0, 0, 0.125);
+    --bs-btn-disabled-color: #F86011;
+    --bs-btn-bg: #f862121A;
+    --bs-btn-disabled-border-color: #F86011;
+    --bs-gradient: none;
+}
+    
 </style>
