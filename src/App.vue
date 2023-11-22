533ce1363f2bf5aed9b086ff721a98d67350d6dc
Trotteriniku <stefanozidda3@gmail.com>
2023-11-22 16:00:32 +0100

    components imported+variables.scss

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <div>
-
-  </div>
+  <HeaderComponent/>
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue';
+import NavComponent from './components/NavComponent.vue';
+import HalfCardComponent from './components/HalfCardComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+import CardNComponent from './components/CardNComponent.vue';
+import CardsComponentList from './components/CardsComponentList.vue';
   export default {
-    import CardNComponent from './components/CardNComponent.vue';
-    import CardsComponentList from './components/CardsComponentList.vue';
-    import FooterComponent from './components/FooterComponent.vue';
-    import HalfCardComponent from './components/HalfCardComponent.vue';
-    import HeaderComponent from './components/HeaderComponent.vue';
-    import NavComponent from './components/NavComponent.vue';
+    
     name:'App',
     components:{
-      CardNComponent,
-      CardsComponentList,
-      FooterComponent,
-      HalfCardComponent,
-      HeaderComponent,
-      NavComponent
-    },
+    CardNComponent,
+    CardsComponentList,
+    FooterComponent,
+    HalfCardComponent,
+    HeaderComponent,
+    NavComponent,
+},
     data(){
       return{
         
