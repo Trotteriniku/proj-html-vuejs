4b94d214fdac380d22d4fbf9240d48ed97a2457f
Trotteriniku <stefanozidda3@gmail.com>
2023-11-23 18:29:11 +0100

    HalfCardComponent fix

--- src/components/HalfCardComponent.vue
+++ src/components/HalfCardComponent.vue
@@ -9,10 +9,13 @@
            
             
             <div class="col-6 p-0 ">
-                <span style="width: 40.8px; height: 40.8px;" class=" rounded-circle  bg-secondary p-2 me-2 ">
-                    <i class="fa-solid fa-chart-simple fa-xs"></i>
-                </span>
-                <span  >{{ store.elements[0].argument }}</span>
+                <div class="d-flex">
+                    <div style="width: 40px; height: 40px;" class=" rounded-circle  bg-primary p-2 me-2 d-flex justify-content-center align-items-center">
+                        <i class="fa-solid fa-chart-simple fa-xs"></i>
+                    </div>
+                    <span  >{{ store.elements[0].argument }}</span>
+                </div>
+                
                 <h2 class="w-50 py-3 my-3">{{ store.elements[0].title }}</h2>
                 <p  class="py-2 my-3">{{ store.elements[0].mainText }}</p>
                 <a class="btn btn-outline-success  my-btn w-50 px-4 py-3 my-3" ><span class="mx-3 fw-medium  ">Get a Consultation</span></a>
@@ -24,10 +27,12 @@
             <img src="/publicImages/img2.svg" alt="image" class="w-100">
             </div>
             <div class="col-6">
-                <span style="max-width: 50px; max-height: 40.8px;"  class=" rounded-circle  bg-secondary p-2 me-2 ">
-                    <i class="fa-solid fa-chart-simple fa-xs " ></i>
-                </span>
-                <span >{{ store.elements[1].argument }}</span>
+                <div class="d-flex">
+                    <div style="width: 40px; height: 40px;"  class=" rounded-circle  bg-warning p-2 me-2 d-flex justify-content-center align-items-center">
+                        <i class="fa-solid fa-chart-simple fa-xs " ></i>
+                    </div>
+                    <span >{{ store.elements[1].argument }}</span>
+                </div>
                 <h2 class="w-50 py-3 my-3">{{ store.elements[1].title }}</h2>
                 <p class="py-2 my-3">{{ store.elements[1].mainText }}</p>
                 <a class="btn btn-outline-success  my-btn w-50 px-4 py-3 my-2" ><span class="mx-3 fw-medium  ">Get a Consultation</span></a>
@@ -40,10 +45,13 @@
             </div>
             
             <div class="col-6">
-                <span style="width: 40.8px; height: 40.8px;" class=" rounded-circle  bg-secondary p-2 me-2">
-                    <i class="fa-solid fa-chart-simple fa-xs"></i>
-                </span>
-                <span >{{ store.elements[2].argument }}</span>
+                <div class="d-flex">
+                    <div style="width: 40px; height: 40px;" class=" rounded-circle  bg-success p-2 me-2 d-flex justify-content-center align-items-center ">
+                        <i class="fa-solid fa-chart-simple fa-xs"></i>
+                    </div>
+                    <span >{{ store.elements[2].argument }}</span>
+                </div>
+                
                 <h2 class="w-50 py-3 my-3">{{ store.elements[2].title }}</h2>
                 <p class="py-2 my-3">{{ store.elements[2].mainText }}</p>
                 <a class="btn btn-outline-success  my-btn w-50 px-4 py-3 my-2 " ><span class="mx-3 fw-medium  ">Get a Consultation</span></a>
